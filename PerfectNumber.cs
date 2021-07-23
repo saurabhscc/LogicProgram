c791f43447085f2028afa7420021176fd191eb76
saurabhs <saurabhpshirsath@gmail.com>
2021-07-23 21:45:02 +0530

    [saurabh] ; Perfect Number is Found

---
+++ PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicProgram
+{
+    class PerfectNumber
+    {
+        public int Number;
+        public int i;
+        public int Result;
+
+        public void CheckPerfectNumber()
+        {
+            Console.WriteLine("Input the  number : ");  
+            Number = int.Parse(Console.ReadLine());
+            Result = 0;//By Default Result is 0
+            Console.WriteLine("The positive divisor  : ");
+            for (i = 1; i < Number; i++) 
+            {
+                if (Number % i == 0) 
+                {
+                    Result = Result + i;  
+                    Console.WriteLine("{0}  ", i); //{0} Format string  
+                }
+            }
+            Console.WriteLine("The sum of the divisor is : {0}", Result);  
+            if (Result == Number)
+                Console.WriteLine("So, the number is perfect.");
+            else
+                Console.WriteLine("So, the number is not perfect.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,26 @@ namespace LogicProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Logic Program ");
+            Console.WriteLine("1 For: Fibonacci Series ");
+            Console.WriteLine("2 For: Perfect Number ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    //Fibonacco Series
+                    FibbonaciSeries FlibSeries = new FibbonaciSeries();
+                    FlibSeries.CheckFibonacciSeries();
+                    break;
+                case 2:
+                    //Perfect Number
+                    PerfectNumber PerfectNum = new PerfectNumber();
+                    PerfectNum.CheckPerfectNumber();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection. Select in b/w  1 and 2");
+                    break;
+            }
+                    Console.ReadLine();
         }
     }
 }
