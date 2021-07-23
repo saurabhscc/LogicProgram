fe9cc6b9f630235d6a1748160d79cd8448c4af3b
saurabhs <saurabhpshirsath@gmail.com>
2021-07-23 20:07:47 +0530

    [saurabh] ;Welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logic Program ");
+        }
+    }
+}
